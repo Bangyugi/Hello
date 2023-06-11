299a68bf95d939505d2f31fa6e03313be46a68a3
Bangyugi <bangtranvan08@gmail.com>
2023-06-12 00:46:11 +0700

    Hello GIthub

---
+++ TestCode.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << " ---------------------------------------- \n";
+    string s = " BangTranVan2004,";
+    s += " hello github workspace";
+    cout << s;
+    cout << "\n ---------------------------------------- ";
+}
